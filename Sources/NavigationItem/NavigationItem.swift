66cf187c53e66620aa9eb199738d505bec83929c
David Walter <hello@davidwalter.at>
2022-07-15 14:59:29 +0200

    Add animated flag

--- Sources/NavigationItem/NavigationItem.swift
+++ Sources/NavigationItem/NavigationItem.swift
@@ -16,20 +16,22 @@ extension View {
     ///
     /// While the `NavigationItem` will still be found without this, it may be delayed or glitchy without this in certain situations.
     ///
+    /// - Parameter animated: Pass `true` to animate the customization; otherwise, pass `false`. Defaults to `true`
     /// - Parameter customize: Callback with the found `UINavigationItem`
-    public func navigationItem(customize: @escaping (UINavigationItem) -> Void) -> some View {
-        modifier(NavigationControllerModifier(customize: customize))
+    public func navigationItem(animated: Bool = true, customize: @escaping (UINavigationItem) -> Void) -> some View {
+        modifier(NavigationControllerModifier(animated: animated, customize: customize))
     }
 }
 
 extension NavigationView {
     /// Access the NavigationItem of the underlying `UINavigationController` and expose it in the `Environment`
     ///
+    /// - Parameter animated: Pass `true` to animate the customization; otherwise, pass `false`. Defaults to `true`
     /// - Parameter customize: Callback with the found `UINavigationItem`
     ///
     /// This is needed on the `NavigationView` in order to be able to expose the `UINavigationController` to all subviews
-    public func navigationItem(customize: ((UINavigationItem) -> Void)?) -> some View {
-        modifier(NavigationControllerModifier(customize: customize, forceEnvironment: true))
+    public func navigationItem(animated: Bool = true, customize: @escaping ((UINavigationItem) -> Void)) -> some View {
+        modifier(NavigationControllerModifier(animated: animated, customize: customize, forceEnvironment: true))
     }
     
     /// Expose the the NavigationItem of the underlying `UINavigationController` in the `Environment`
@@ -40,13 +42,64 @@ extension NavigationView {
     ///
     /// This is needed on the `NavigationView` in order to be able to expose the `UINavigationController` to all subviews
     public func navigationItem() -> some View {
-        modifier(NavigationControllerModifier(customize: nil, forceEnvironment: true))
+        modifier(NavigationControllerModifier(animated: false, customize: nil, forceEnvironment: true))
     }
 }
 
+#if swift(>=5.7)
+@available(iOS 16.0, macOS 13.0, tvOS 16.0, watchOS 9.0, *)
+extension NavigationStack {
+    /// Access the NavigationItem of the underlying `UINavigationController` and expose it in the `Environment`
+    ///
+    /// - Parameter animated: Pass `true` to animate the customization; otherwise, pass `false`. Defaults to `true`
+    /// - Parameter customize: Callback with the found `UINavigationItem`
+    ///
+    /// This is needed on the `NavigationView` in order to be able to expose the `UINavigationController` to all subviews
+    public func navigationItem(animated: Bool = true, customize: @escaping ((UINavigationItem) -> Void)) -> some View {
+        modifier(NavigationControllerModifier(animated: animated, customize: customize, forceEnvironment: true))
+    }
+    
+    /// Expose the the NavigationItem of the underlying `UINavigationController` in the `Environment`
+    ///
+    /// While not strictly necessary to expose the the underlying `UINavigationController`
+    /// in the `Environment` it is advised to do so as it heavily simplifies finding the `UINavigationController`. Without exposure accessing the NavigationItem might be slightly
+    /// delayed and may cause glitches.
+    ///
+    /// This is needed on the `NavigationStack` in order to be able to expose the `UINavigationController` to all subviews
+    public func navigationItem() -> some View {
+        modifier(NavigationControllerModifier(animated: false, customize: nil, forceEnvironment: true))
+    }
+}
+
+@available(iOS 16.0, macOS 13.0, tvOS 16.0, watchOS 9.0, *)
+extension NavigationSplitView {
+    /// Access the NavigationItem of the underlying `UINavigationController` and expose it in the `Environment`
+    ///
+    /// - Parameter animated: Pass `true` to animate the customization; otherwise, pass `false`. Defaults to `true`
+    /// - Parameter customize: Callback with the found `UINavigationItem`
+    ///
+    /// This is needed on the `NavigationView` in order to be able to expose the `UINavigationController` to all subviews
+    public func navigationItem(animated: Bool = true, customize: @escaping ((UINavigationItem) -> Void)) -> some View {
+        modifier(NavigationControllerModifier(animated: animated, customize: customize, forceEnvironment: true))
+    }
+    
+    /// Expose the the NavigationItem of the underlying `UINavigationController` in the `Environment`
+    ///
+    /// While not strictly necessary to expose the the underlying `UINavigationController`
+    /// in the `Environment` it is advised to do so as it heavily simplifies finding the `UINavigationController`. Without exposure accessing the NavigationItem might be slightly
+    /// delayed and may cause glitches.
+    ///
+    /// This is needed on the `NavigationSplitView` in order to be able to expose the `UINavigationController` to all subviews
+    public func navigationItem() -> some View {
+        modifier(NavigationControllerModifier(animated: false, customize: nil, forceEnvironment: true))
+    }
+}
+#endif
+
 struct NavigationControllerModifier: ViewModifier {
+    var animated: Bool
     let customize: ((UINavigationItem) -> Void)?
-    var forceEnvironment: Bool = false
+    var forceEnvironment = false
 
     @Environment(\.navigationController) var navigationController
     @State private var holder: UINavigationController?
@@ -59,13 +112,14 @@ struct NavigationControllerModifier: ViewModifier {
                         // RootViewContoller is still the visibleViewController, try again delayed
                         DispatchQueue.main.async {
                             guard let viewController = navigationController.visibleViewController else { return }
-                            customize?(viewController.navigationItem)
+                            customize(navigationItem: viewController.navigationItem)
                         }
                     } else {
                         guard let viewController = navigationController.visibleViewController else { return }
-                        customize?(viewController.navigationItem)
+                        customize(navigationItem: viewController.navigationItem)
                     }
                 }
+                .animation(.default, value: forceEnvironment)
         } else {
             content
                 .overlay(overlay)
@@ -73,21 +127,32 @@ struct NavigationControllerModifier: ViewModifier {
         }
     }
     
-    var overlay: some View {
-        FindNavigationController {
-            holder = $0
-            callback()
-        }
-        .frame(width: 0, height: 0)
-        .onAppear {
-            callback()
+    private func customize(navigationItem: UINavigationItem) {
+        if animated {
+            UIView.animate(withDuration: 0.35) {
+                customize?(navigationItem)
+            }
+        } else {
+            customize?(navigationItem)
         }
     }
     
-    func callback() {
+    var overlay: some View {
+        FindNavigationController {
+            holder = $0
+            customizeOverlay()
+        }
+        .frame(width: 0, height: 0)
+        .onAppear {
+            customizeOverlay()
+        }
+    }
+    
+    
+    func customizeOverlay() {
         DispatchQueue.main.async {
             guard let item = holder?.children.last?.navigationItem else { return }
-            customize?(item)
+            customize(navigationItem: item)
         }
     }
 }
